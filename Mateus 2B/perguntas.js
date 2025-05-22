b9ac65b69e8e179bfaa479c814beee2291ec3d83
mateussilva-matt <eleuterio.mateus@escola.pr.gov.br>
2025-05-22 14:23:36 -0300

    Add files via upload

---
+++ Mateus 2B/perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    'História',
+    'Quais são as divisões da Pré-História?',
+    'Paleolítico, Mesolítico e Neolítico.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Júlio César?',
+    'Caio Júlio César foi um patrício, líder militar e político romano. Desempenhou um papel crítico na transformação da República Romana no Império Romano.'
+)
