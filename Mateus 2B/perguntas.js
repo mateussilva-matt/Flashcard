b7eaeb055666ed77a62836e7be983f04d0418ba7
mateussilva-matt <eleuterio.mateus@escola.pr.gov.br>
2025-05-29 14:00:12 -0300

    Add files via upload

--- Mateus 2B/perguntas.js
+++ Mateus 2B/perguntas.js
@@ -9,3 +9,9 @@ criaCartao(
     'Quem foi Júlio César?',
     'Caio Júlio César foi um patrício, líder militar e político romano. Desempenhou um papel crítico na transformação da República Romana no Império Romano.'
 )
+
+criaCartao(
+    'História',
+    'Qual era o tipo de governo da União Soviética?',
+    'A União Soviética foi um governo Socialista.'
+)
